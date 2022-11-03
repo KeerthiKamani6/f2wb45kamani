f739bb9eeb4b678d52a3971b31a58cdcac9f8540
KeerthiKamani6 <92873846+KeerthiKamani6@users.noreply.github.com>
2022-11-02 22:07:10 -0500

    updated

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var penRouter = require('./routes/pen');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -23,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pen', penRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
